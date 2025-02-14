cdd1cf5a2f004d6e487ed280765cea28ec973bfe
Bahaa eddine hammadi <b.hammadi@esi-sba.dz>
2025-02-14 17:42:33 +0100

    Client_tcp et serveur tcp terminé, phase 1 fini

--- Phase1/client_udp.py
+++ Phase1/client_udp.py
@@ -1,9 +1,9 @@
 import socket
 
-def main():
-    server_address = ('localhost', 8081)  
 
-    
+def main():
+    server_address = ('localhost', 8081)  # Adresse du serveur UDP (doit correspondre au serveur)
+
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         while True:
             message = input("Entrez le message à envoyer (ou 'exit' pour quitter) : ")
@@ -11,12 +11,13 @@ def main():
                 print("Fermeture du client.")
                 break
             try:
-                
+
                 print(f"Envoi de {message} au serveur {server_address}")
-                sent = sock.sendto(message.encode(), server_address)
+                sock.sendto(message.encode(), server_address)  # Envoie le message au serveur
 
             except Exception as e:
                 print(f"Erreur: {e}")
+
 
 if __name__ == "__main__":
     main()
--- Phase1/server.py
+++ Phase1/server.py
@@ -4,8 +4,18 @@ import threading
 
 def handle_tcp_client(client_socket, addr):
     with client_socket:
-        message = client_socket.recv(1024).decode('utf-8')
-        print(f"Send with TCP from IP : {addr[0]} with message {message}")
+        try:
+            while True:
+                message = client_socket.recv(1024).decode('utf-8')
+                if not message:
+                    break
+                print(f"Send with TCP from IP : {addr[0]} with message {message}")
+                # Répondre au client
+                réponse = f"Message {message} à été bien reçu"
+                client_socket.sendall(réponse.encode())
+        except Exception as e:
+            print(f"Erreur avec le client {addr[0]} : {e}")
+
 
 def tcp_server():
     tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,6 +27,7 @@ def tcp_server():
         client_handler = threading.Thread(target=handle_tcp_client, args=(client_socket, addr))
         client_handler.start()
 
+
 def udp_server():
     udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_sock.bind(('0.0.0.0', 8081))
@@ -24,6 +35,7 @@ def udp_server():
     while True:
         message, addr = udp_sock.recvfrom(1024)
         print(f"Send with UDP from IP : {addr[0]} with message {message.decode('utf-8')}")
+
 
 tcp_thread = threading.Thread(target=tcp_server)
 udp_thread = threading.Thread(target=udp_server)
