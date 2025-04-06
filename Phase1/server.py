096b264437a83b007e6da36e15040f7fdcbe7de8
Redak92 <134450370+Redak92@users.noreply.github.com>
2025-04-06 13:14:46 +0200

    update

--- Phase1/server.py
+++ Phase1/server.py
@@ -1,47 +1,51 @@
-import socket
-import threading
+L'aphorisme de cette année, est loin d'être ridicule.	
+#Laissons la voie libre à ceux qui peuvent tracer la route
 
-# Serveur TCP/UDP
-def handle_tcp_client(client_socket, addr):
-    with client_socket:
-        try:
-            while True:
-                message = client_socket.recv(1024).decode('utf-8')
-                if not message:
-                    break
-                print(f"Send with TCP from IP : {addr[0]} with message {message}")
-                # Répondre au client
-                réponse = f"Message {message} à été bien reçu"
-                client_socket.sendall(réponse.encode())
-        except Exception as e:
-            print(f"Erreur avec le client {addr[0]} : {e}")
+Le sport, la musique, les sciences, la politique !
+[2]
+Ces domaines n'ont pas grand chose en commun mais ils se rassemblent autour d'une chose, [2] ils sont en mouvement.
 
+Il y a un an de ça, j'étais parti, pour ceux qui connaissent, au CNAM à Paris.
+Pour ceux qui connaissent pas c'est une fac, avec le musée des Arts et Métiers au même endroit.
+J'y étais allé voir une conférence sur les réseaux de télécommunications, je venais de commencer mes études en informatique.
+À la fin on finissait par une session de questions/réponses, Moi j'en profitais pour avoir mes premières prises de parole en public, ça me plaisait beaucoup.
+Quand j'ai finis de parler il y a une dame qui s'était assise tout devant qui a pris le micro. Là où la majorité des personnes dans la salle étaient des étudiants
+ou des professeurs, elle était la seule personne âgée à être présente, mais j'ai trouvé ça génial, j'étais admiratif de la passion qu'elle avait pour venir dans des 
+conférences qui se terminent à 22h.
+Et, hmm, elle a fait une intervention très confuse, personne n'a vraiment compris sa question, elle continuait de parler sans vraiment de lien entre ses phrases.
+Et pourtant on a tous vite réalisé de quoi il s'agissait. Elle n'avait pas de question, elle voulait s'exprimer, elle en en avait besoin.
 
-def tcp_server():
-    tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_sock.bind(('0.0.0.0', 8080))
-    tcp_sock.listen(5)
-    print("TCP server listening on port 8080")
-    while True:
-        client_socket, addr = tcp_sock.accept()
-        client_handler = threading.Thread(target=handle_tcp_client, args=(client_socket, addr))
-        client_handler.start()
+C'est une retraitée, qui a fait toute sa carrière dans la recherche, notamment en électronique. Et le projet de sa vie s'appelait Cyclades. C'était un projet de réseaux
+télécom, le but c'était de voir si on pouvait faire communiquer des ordinateurs à grande distance en les branchant avec des vrais câbles. C'était dans les années 70, à 
+l'époque où tout le monde jurait par le sans-fil, surtout que la télé venait d'arriver, alors c'était LE sujet du moment parmi les chercheurs. Et elle, elle est était là
+avec sa clique de vingt ingénieurs, dans les locaux de l'INRIA, avec tout le monde qui lui faisait bien comprendre qu'elle est juste un quota. On a alloué un budget
+à ce domaine donc vous êtes là, mais bon, vous êtes bloqués dans le passé. Vous allez faire quoi ? Avec vos grosses boîtes noires et vos centaines de 
+câbles ? Vous allez en mettre partout ? Un peu de sérieux s'il vous plaît.
 
+Imaginez alors la réaction des gérants quand elle a dû leur expliquer qu'elle n'avait pas assez d'argent pour avoir des résultats concrets.
 
-def udp_server():
-    udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_sock.bind(('0.0.0.0', 8081))
-    print("UDP server listening on port 8081")
-    while True:
-        message, addr = udp_sock.recvfrom(1024)
-        print(f"Send with UDP from IP : {addr[0]} with message {message.decode('utf-8')}")
+Mais le gars il a entendu "pas assez de budget" il a pas cherché à comprendre, il lui a dit "bon, vous prenez vos affaires, vous vous cassez,je vous laisse quelques mois pour finir de publier vos papiers et bonne continuation.
 
+Ils étaient les 20 fous dans une salle bruyante, ils faisaient péter la facture d'électricité enfin bref, tout est à oublier dans cette histoire.
 
-tcp_thread = threading.Thread(target=tcp_server)
-udp_thread = threading.Thread(target=udp_server)
+Sauf que, hum, il y en a qui n'ont pas oublié. Alors j'imagine que Cyclades ça ne vous dit pas grand chose, mais - même si c'est assez niche - peut être que ArpaNET un peu plus.
+C'était une équipe américaine, concurrente, qui travaillait sur les mêmes choses. Et, quand elle a entendu la nouvelle, que la petite équipe de français, qui arrivait à les devancer avec un petit budget, se faisait virer ?? 
 
-tcp_thread.start()
-udp_thread.start()
+Il a suffit d'un appel, 
+Et dans cet appel, 
+Il n'a suffit que de trois mots : "Je vous crois"
 
-tcp_thread.join()
-udp_thread.join()
+Sur la page Wikipédia du projet Cyclades, il y a marqué "concurrent et partenaire du projet ArpaNET". Mais c'est faux, 
+c'est un concurrent PUIS partenaire du projet ArpaNET.
+
+Sur la page du projet ArpaNET, il y a marqué "ancêtre et créateur d'Internet".
+
+Quand on d'où ça part,
+On a le droit de se dire qu'Internet serait né en France
+On aurait peut être pu dire "Pas mal non ? C'est français"
+
+L'histoire regorge de génies qu'on prenait pour des fous. Galilée avec sa terre ronde, Albert Einstein, Max Planck, Socrate, Darwin, Louis Pasteur, Steve Jobs
+
+Évidemment il n'est pas question de permanente révolution
+Notre maire pourra vous en parler mieux que moi il est élu depuis 25 ans
+
