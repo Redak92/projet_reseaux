7be34279dd791e8ceab5d187c75d7dd83bfb83fb
Abdel <abdelhakmerabtene@gmail.com>
2025-04-10 18:05:54 +0200

    Attribution de l'@ip du serveur à file_sender_tcp

--- Phase1/client/file_sender_tcp.py
+++ Phase1/client/file_sender_tcp.py
@@ -1,7 +1,7 @@
 import socket
 
 
-def send_file(filename, server_address=('localhost', 9000)):
+def send_file(filename, server_address=('192.168.11.129', 9000)): # Adresse IP de ma VM2 qui joue le role du serveur
     with open(filename, 'rb') as f, socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect(server_address)
         print(f"Envoi du fichier {filename}")
