989a9d5ac4483ac2036b1bdcbc277054b19a469c
Abdel <abdelhakmerabtene@gmail.com>
2025-04-12 15:35:35 +0200

    file_receiver_tcp reste toujours en écoute pour tests DoS

--- Phase1/server/file_receiver_tcp.py
+++ Phase1/server/file_receiver_tcp.py
@@ -1,18 +1,20 @@
 import socket
 
-
-def receive_file(save_path='../received/received_file.pdf', server_address=('0.0.0.0', 9000)):
+def receive_file(server_address=('0.0.0.0', 9000)):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.bind(server_address)
-        sock.listen(1)
-        print('Waiting for connexion')
-        conn, addr = sock.accept()
-        print(f"Connexion de {addr}")
-        with open(save_path, 'wb') as f:
-            while data := conn.recv(1024):
-                f.write(data)
-        print("Fichier reçu.")
+        sock.listen(5)
+        print(f"Serveur en écoute sur {server_address[0]}:{server_address[1]}")
 
+        while True:
+            print("En attente d'une connexion entrante...")
+            conn, addr = sock.accept()
+            print(f"Connexion reçue de {addr}")
+            with conn:
+                with open(f"received_from_{addr[1]}.pdf", 'wb') as f:
+                    while data := conn.recv(1024):
+                        f.write(data)
+                print("Fichier reçu.")
 
 if __name__ == "__main__":
     receive_file()
