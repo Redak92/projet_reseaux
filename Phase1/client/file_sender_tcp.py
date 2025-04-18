54b39c98fcc9f88ca73e4214f2935638a98f681e
trash <trash@harmon.home>
2025-04-18 20:23:20 +0200

    update

--- Phase1/client/file_sender_tcp.py
+++ Phase1/client/file_sender_tcp.py
@@ -1,14 +1,19 @@
-import socket
+from protocols.TCP import TCPSocket
 
+HOST_ADDR = "192.168.10.1"
+HOST_PORT = 12345
 
-def send_file(filename, server_address=('192.168.11.129', 9000)): # Adresse IP de ma VM2 qui joue le role du serveur
-    with open(filename, 'rb') as f, socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect(server_address)
+def send_file(filename, server_address=('192.168.10.2', 9000)):  # Adresse IP de ma VM2 qui joue le role du serveur
+    sock = TCPSocket(HOST_ADDR, HOST_PORT)
+    with open(filename, 'rb') as f:
+        settings = sock.handshake(server_address[0], server_address[1])
         print(f"Envoi du fichier {filename}")
         while chunk := f.read(1024):
-            sock.sendall(chunk)
+            settings = sock.send_data(server_address[0], server_address[1], settings[0], settings[1], settings[2], chunk)
         print("Fichier envoyé.")
+
+    sock.end_tcp(server_address[0], server_address[1], settings[0] + 1, settings[1], settings[2])
 
 
 if __name__ == "__main__":
-    send_file("../Media/Projet L3.pdf")
+    send_file("Media/Projet L3.pdf")
