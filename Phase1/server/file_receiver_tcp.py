b6d8b7888d1d55d90e261ad9c251db927a719ccb
Abdel <abdelhakmerabtene@gmail.com>
2025-04-26 00:07:43 +0200

    Correction dossier de réception fichier

--- Phase1/server/file_receiver_tcp.py
+++ Phase1/server/file_receiver_tcp.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 def receive_file(server_address=('0.0.0.0', 9000)):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -6,15 +7,18 @@ def receive_file(server_address=('0.0.0.0', 9000)):
         sock.listen(5)
         print(f"Serveur en écoute sur {server_address[0]}:{server_address[1]}")
 
+        os.makedirs("../received", exist_ok=True)
+
         while True:
             print("En attente d'une connexion entrante...")
             conn, addr = sock.accept()
             print(f"Connexion reçue de {addr}")
             with conn:
-                with open(f"received_from_{addr[1]}.pdf", 'wb') as f:
+                file_path = f"../received/received_from_{addr[1]}.pdf"
+                with open(file_path, 'wb') as f:
                     while data := conn.recv(1024):
                         f.write(data)
-                print("Fichier reçu.")
+                print(f"Fichier reçu et enregistré dans {file_path}")
 
 if __name__ == "__main__":
     receive_file()
